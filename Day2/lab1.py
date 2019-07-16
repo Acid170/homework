8497becae19ac56819f7434ff0610c5cd1dba7fe
Acid170 <eliezergyan170@gmail.com>
2019-07-16 17:43:35 +0000

    I have added lab1

---
+++ Day2/lab1.py
@@ -0,0 +1,29 @@
+#1
+print ("Python is a scripting language")
+#2
+print ('I love python programming language')
+#3
+multiple_lines_string = '''Hi,
+I'm Eliezer
+Nice meeting you'''
+print(multiple_lines_string)
+#4
+single_line_span = '''Hi,\
+ I'm Eliezer,\
+ Nice meeting you'''
+print(single_line_span)
+#5
+string_length = "Eliezer"
+print(len(string_length))
+#6
+string_0ne = "Concat"
+string_two = "enation"
+print(string_0ne + string_two)
+#7
+variable_one = "Kofi"
+variable_two = "Ama"
+print(variable_one+" , "+variable_two)
+#8
+full_string = "bazinga"
+substring = (full_string[2:6])
+print(substring)
